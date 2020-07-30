371ef21169448f45c783ad91d3cd7f3ede1a399c
Arsh <arshagarwal.18@gmail.com>
2020-07-30 17:08:14 +0530

    check fix

--- train2.py
+++ train2.py
@@ -74,7 +74,7 @@ for i in range(opt.epochs):
             forward_v=model(test_set[it:it+batch_size])
             loss_v=tf.keras.losses.MeanSquaredError()(y_test[it:it+batch_size],forward_v)
             check=tf.reduce_sum((y_test[it:it+batch_size] - forward_v)**2)/batch_size
-            assert loss_v== check print("check : {} loss_v: {} ".format(check,loss_v))
+            assert loss_v== check ,"check : {} loss_v: {} ".format(check,loss_v)
             loss_vt+=loss_v
 
         grads=tape.gradient(loss,model.trainable_variables)
