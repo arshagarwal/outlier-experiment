a5e1737726fda87df7319788ccfbea5d541cd742
Arsh <arshagarwal.18@gmail.com>
2020-07-30 15:13:03 +0530

    rounding losses

--- train.py
+++ train.py
@@ -87,7 +87,7 @@ for i in range(opt.epochs):
     loss_vt/=n_batches
     loss_t=tf.reduce_sum(loss_t)
     loss_vt=tf.reduce_sum(loss_vt)
-    print("Loss: {} Validation loss:{} ".format(loss_t,loss_vt))
+    print("Loss: {} Validation loss:{} ".format(round(loss_t,2),round(loss_vt,2)))
 
 
 
