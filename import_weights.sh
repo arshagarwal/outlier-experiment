182ab977df9c66c07ce500953a8ff85111312afa
arshagarwal <31827288+arshagarwal@users.noreply.github.com>
2020-07-31 10:59:16 +0530

    Update import_weights.sh

--- import_weights.sh
+++ import_weights.sh
@@ -1,4 +1,5 @@
 # Code to import model and  weight file from drive for face_recognition.py
+mkdir Facenet
 wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1PZ_6Zsy1Vb0s0JmjEmVd8FS99zoMCiN1' -O facenet_model.h5
 wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1e6PHRlIeayAsvRGpYUwvstklvJy-3H5B' -O facenet_weights.h5
 mv facenet_weights.h5 Facenet
