256c3557bd7cbbbf5cf498653afdcdb527e97789
Arsh <arshagarwal.18@gmail.com>
2020-08-01 13:25:27 +0530

    keras model

---
+++ train_slim2.py
@@ -0,0 +1,35 @@
+"""
+Code to tarin on the slim_dataset using outliers
+"""
+from models.Face_recognition import FR_model
+import os
+from PIL import Image
+import numpy as np
+from Options.slim_train_options import options
+import tensorflow as tf
+from models.model import model
+import utils
+
+opt=options().parser
+s_e_freq=opt.s_e_freq
+
+path='slim_dataset/Train_dataset'
+train_set,y_train=utils.process(path)
+
+
+FR=FR_model()
+model=model()
+
+# getting corresponding embeddings
+train_set=FR(train_set)
+
+optimizer=tf.keras.optimizers.Adam()
+batch_size=opt.b_size
+n_batches = int(len(train_set) / opt.b_size)
+
+checkpoint=tf.train.Checkpoint(model=model,opt=optimizer)
+manager=tf.train.CheckpointManager(checkpoint,directory='Checkpoints',max_to_keep=3)
+
+model.compile(optimizer=tf.keras.optimizers.Adam(),loss=tf.keras.losses.MeanAbsoluteError())
+model.fit(train_set,y_train,opt.b_size,epochs=opt.epochs)
+
