64d32d964cd3558eaea4dc6769075664051ed7fe
Arsh <arshagarwal.18@gmail.com>
2020-11-01 22:28:23 +0530

    adding move.sh

---
+++ move.sh
@@ -0,0 +1,48 @@
+mkdir cacd
+mkdir cacd/10-30
+mkdir cacd/50+
+
+mv CACD/21/* cacd/10-30
+mv CACD/22/* cacd/10-30
+mv CACD/23/* cacd/10-30
+mv CACD/24/* cacd/10-30
+mv CACD/25/* cacd/10-30
+mv CACD/26/* cacd/10-30
+mv CACD/27/* cacd/10-30
+mv CACD/28/* cacd/10-30
+mv CACD/29/* cacd/10-30
+mv CACD/30/* cacd/10-30
+mv CACD/11/* cacd/10-30
+mv CACD/12/* cacd/10-30
+mv CACD/13/* cacd/10-30
+mv CACD/14/* cacd/10-30
+mv CACD/15/* cacd/10-30
+mv CACD/16/* cacd/10-30
+mv CACD/17/* cacd/10-30
+mv CACD/18/* cacd/10-30
+mv CACD/19/* cacd/10-30
+mv CACD/20/* cacd/10-30
+
+mv CACD/50/* cacd/50+
+mv CACD/51/* cacd/50+
+mv CACD/52/* cacd/50+
+mv CACD/53/* cacd/50+
+mv CACD/54/* cacd/50+
+mv CACD/55/* cacd/50+
+mv CACD/56/* cacd/50+
+mv CACD/57/* cacd/50+
+mv CACD/58/* cacd/50+
+mv CACD/59/* cacd/50+
+mv CACD/60/* cacd/50+
+mv CACD/61/* cacd/50+
+mv CACD/62/* cacd/50+
+mv CACD/63/* cacd/50+
+mv CACD/64/* cacd/50+
+mv CACD/65/* cacd/50+
+mv CACD/66/* cacd/50+
+mv CACD/67/* cacd/50+
+mv CACD/68/* cacd/50+
+mv CACD/69/* cacd/50+
+mv CACD/70/* cacd/50+
+mv CACD/71/* cacd/50+
+
